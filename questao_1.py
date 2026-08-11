a107ee1105e9c4ec0136cbcf7a4b487176a76daa
carol-xavier <xavier.biotec@gmail.com>
2026-08-11 12:59:07 -0300

    Evaluation of 'total' column

--- questao_1.py
+++ questao_1.py
@@ -33,3 +33,24 @@ Pela informação acima, podemos ver que a base de dados possui 48.998 linhas e 
 Em salesperson_id, 24131 valores nulos foram encontrados, e os tipos de dados das colunas são variados, incluindo inteiros, floats e objetos.
 Além disso, a média está bem acima da mediana (25.917,84), o que sugere uma distribuição assimétrica à direita.
 """
+# %%
+"""
+=============================================================================
+1.2 Avaliação da coluna "total"
+=============================================================================
+"""
+# %%
+total_duplicates = df_orders.duplicated().sum() # Verificar duplicidade de registros completos.
+print(f"Total duplicate rows: {total_duplicates}")
+
+# %%
+fisrt_date = df_orders['created_at'].min() # Verificar a primeira data de criação de pedidos    
+last_date = df_orders['created_at'].max() # Verificar a última data de criação de pedidos
+print(f"O intervalo de tempo da base de dados é de {fisrt_date} até {last_date}.") # Imprimir o intervalo de tempo da base de dados
+
+# %%
+max_value = df_orders['total'].max() # Verificar o valor máximo de pedidos
+min_value = df_orders['total'].min() # Verificar o valor mínimo de pedidos
+avg_ticket = df_orders['total'].mean() # Verificar o valor médio de pedidos 
+print(f"O valor máximo de pedidos é de R$ {max_value:.2f}, o valor mínimo de pedidos é de R$ {min_value:.2f} e o valor médio é R$ {avg_ticket:.2f}.") # Imprimir os valores máximo e mínimo de pedidos
+

561baa26cbb53126958e8493c33190183378364c
carol-xavier <xavier.biotec@gmail.com>
2026-08-12 14:28:08 -0300

    Functions to load csv data into postgres db using psycopg library and further instructions.

--- questao_3.py
+++ questao_3.py
@@ -39,3 +39,52 @@ def get_csv_columns(file_path):
     with open(file_path, "r", encoding="utf-8", newline="") as file:
         reader = csv.reader(file)
         return next(reader)
+
+## Previamente, um banco de dados postgreSQL foi criado pelo terminal, utilizando os comandos:
+### CREATE USER username WITH PASSWORD 'password';
+### CREATE DATABASE nome_do_banco;
+## O schema.sql de questao_2.py foi executado no banco de dados para criar as tabelas necessárias, comando:
+### psql -h db_host -U db_user -d db_name -f schema.sql
+
+# Funções para carregar os dados de um arquivo CSV em uma tabela do PostgreSQL
+def load_csv(conn, file_path, table_name):
+    columns = get_csv_columns(file_path)
+
+    copy_query = sql.SQL(
+        "COPY {} ({}) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)"
+    ).format(
+        sql.Identifier(table_name),
+        sql.SQL(", ").join(map(sql.Identifier, columns)),
+    )
+
+    with open(file_path, "r", encoding="utf-8", newline="") as file:
+        with conn.cursor() as cur:
+            with cur.copy(copy_query) as copy:
+                while data := file.read(8192):
+                    copy.write(data)
+
+
+def main():
+    csv_files = sorted(
+        file_name
+        for file_name in os.listdir(CSV_DIR)
+        if file_name.lower().endswith(".csv")
+    )
+
+    with psycopg.connect(**DB_CONFIG) as conn:
+        for file_name in csv_files:
+            table_name = os.path.splitext(file_name)[0]
+            file_path = os.path.join(CSV_DIR, file_name)
+
+            load_csv(conn, file_path, table_name)
+
+            print(f"{file_name} carregado em {table_name}")
+
+    print("Carregamento concluído.")
+
+
+if __name__ == "__main__":
+    main()
+
+# Run the comand below to execute the script (in the terminal, in the same directory as the script):
+# python questao_3.py

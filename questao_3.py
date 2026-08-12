d916c7b2d780ab64194c1dcf7475600d63e68627
carol-xavier <xavier.biotec@gmail.com>
2026-08-12 14:18:11 -0300

    Import necessary libs, .env variables, and perform DB conection.

---
+++ questao_3.py
@@ -0,0 +1,32 @@
+"""
+=============================================================================
+Questão 3 - Load Data: Criar e carregar tabelas no PostgreSQL a partir de arquivos CSV
+=============================================================================
+"""
+
+#Importando as bibliotecas necessárias
+import csv
+import os
+import psycopg
+from psycopg import sql
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Importando as variáveis de ambiente do arquivo .env
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+# Configuração do banco de dados para conexão
+DB_CONFIG = {
+    "dbname": DB_NAME,
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+    "host": DB_HOST,
+    "port": DB_PORT,
+}
